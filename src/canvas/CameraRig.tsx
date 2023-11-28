2de51f866cf6ab6ddb30cf8ce09876f3d4c66c5f
Mannas <mannasnarang8@gmail.com>
2023-11-28 19:22:14 +0530

    new commit

--- src/canvas/CameraRig.tsx
+++ src/canvas/CameraRig.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
 
-const CameraRig = () => {
+import state from '../store';
+
+const CameraRig = ({children}) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    )
+  })
+  // setting the model rotation smoothly
+
+
+  
   return (
-    <div>CameraRig</div>
+    <group ref={group}>
+      {children}
+    </group>
   )
 }
 
--- src/canvas/index.tsx
+++ src/canvas/index.tsx
@@ -1,11 +1,25 @@
 import { Environment, Center } from '@react-three/drei'
 import {Canvas} from '@react-three/fiber' 
 
+import Shirt from './Shirt'
+import Backdrop from './Backdrop'
+import CameraRig from './CameraRig'
+
+
+
 const CanvasModel = () => {
   return (
-    <div>
-    
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+
+      <CameraRig>
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
   )
 }
 
